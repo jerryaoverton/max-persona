dd8b12383cbb8661fc7404660ab9b56a9f8b7c36
Jerry Overton <jerryaoverton@yahoo.com>
2020-12-29 08:23:49 -0600

    added the ability to converse

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import os
-import arrow  # normalizing dates
-from simpletransformers.conv_ai import ConvAIModel
+from simpletransformers.conv_ai import ConvAIModel, ConvAIArgs
+
 
 # needed to launch the app
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# holds the short-term memory for the AI
-stm = []
 
+# define arguments for the speech response model creation
+model_args = ConvAIArgs()
+# for formulating the speech response, consider the last 5 user interactions
+model_args.max_history = 5
 # the model needed to formulate speech responses
-model = ConvAIModel("gpt", "gpt_personachat_cache")
+model = ConvAIModel("gpt", "static/language_model/", use_cuda=False, args=model_args)
+
+
+# create the ai persona
+personality = [
+    "My name is Max.",
+    "I'm a machine'.",
+    "I was created by Jerry Overton.",
+]
+
+# define the interaction history
+history = [
+    "Hello, what's your name?",
+    "Max",
+    "What do you do for a living?",
+    "I'm an artificial intelligence",
+]
 
 
 @app.route('/')
@@ -20,38 +38,15 @@ def index():
     return render_template("index.html")
 
 
-# clears the memory of anything that has gone stale
-def clear_memory(memory):
-    memory = [dialog for dialog in memory if not stale_dialog(dialog)]
-
-
-# determines if a piece of dialog happened long enough in the past to be forgotten
-def stale_dialog(dialog):
-    # the recent past is anything later than 5 minutes ago
-    now = arrow.utcnow()
-    recent_past = now.shift(minutes=-5)
-
-    # dialog is stale if it is older than the recent past
-    is_stale = dialog["timestamp"] < recent_past
-
-    return is_stale
-
-
 @socketio.on('signal')
 def handle_signal(msg):
-    # clear short-term memory of any old dialog
-    clear_memory(stm)
+    global history
 
-    # turn the message received into part of an ongoing dialog
-    # by adding a timestamp and a source
-    dt = arrow.utcnow()
-    dialog = {"content": msg, "timestamp": dt, "source": "user"}
+    # try the model
+    response, history = model.interact_single(msg, history, personality=personality)
 
-    # add the recent dialog to the short-term memory
-    stm.append(dialog)
-    print(stm)
-
-    socketio.emit("response", "hello from the server")
+    socketio.emit("response", response)
+    print(history)
 
 
 if __name__ == "__main__":
