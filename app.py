10f666472e669ee0b66e39e4b67b15000b7f2546
Jerry Overton <jerryaoverton@yahoo.com>
2021-06-05 08:36:48 -0500

    updated persona

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import os
 from simpletransformers.conv_ai import ConvAIModel, ConvAIArgs
-
+import requests
+import json
 
 # needed to launch the app
 app = Flask(__name__)
@@ -42,10 +43,21 @@ def index():
 def handle_signal(msg):
     global history
 
-    # try the model
-    response, history = model.interact_single(msg, history, personality=personality)
+    # run the message into the behavior
+    url = 'http://localhost:5005/webhooks/rest/webhook'
+    params = "{\"sender\":\"default\",\"message\":\"" + msg + "\"}"
 
-    socketio.emit("response", response)
+    req = requests.post(url, data=params, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    data = req.json()
+    response = data[0]['text']
+
+    if response != "":
+        socketio.emit("response", response)
+    else:
+        # if the behavior doesn't recognise the message, try the model
+        response, history = model.interact_single(msg, history, personality=personality)
+        socketio.emit("response", response)
+
     print(history)
 
 
